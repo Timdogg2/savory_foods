56894114ad06e9ab1a8cdbe2ac0faa02bea30742
waretim <82613259+waretim@users.noreply.github.com>
2021-04-20 11:23:10 -0400

    Add files via upload

--- application/config.php
+++ application/config.php
@@ -1,7 +1,22 @@
 <?php
 /**
- * Author: Sydney Weddell
- * Date: 4/15/2021
+ * Author: Timothy Ware Jr.
+ * Date: 4/19/2021
  * File: config.php
  * Description:
  */
+
+error_reporting(E_ALL);
+
+//local time zone
+date_default_timezone_set('America/New_York');
+
+//base url of the application
+define("BASE_URL", "http://localhost/I211/savory-foods");
+
+/*************************************************************************************
+ *                       settings for Recipes                                       *
+ ************************************************************************************/
+
+//define default path for media images
+define("FOOD_IMG", "www/img/");
--- application/database.class.php
+++ application/database.class.php
@@ -1,12 +1,63 @@
 <?php
 /**
- * Author: Sydney Weddell
- * Date: 4/15/2021
+ * Author: Tim Ware Jr.
+ * Date: 4/19/2021
  * File: database.class.php
  * Description:
  */
 
 class database
 {
+//define database parameters
+    private $param = array(
+    'host' => 'localhost',
+    'login' => 'phpuser',
+    'password' => 'phpuser',
+    'database' => 'savory_foods',
+    'tblCategories' => 'categories',
+    'tblRecipes' => 'recipes',
+    'tblUsers' => 'users',
+);
+    //define the database connection object
+    private $objDBConnection = NULL;
+    static private $_instance = NULL;
 
+    //constructor
+    private function __construct() {
+    $this->objDBConnection = @new mysqli(
+        $this->param['host'], $this->param['login'], $this->param['password'], $this->param['database']
+    );
+    if (mysqli_connect_errno() != 0) {
+        $message = "Connecting database failed: " . mysqli_connect_error() . ".";
+        include 'error.php';
+        exit();
+    }
+}
+
+    //static method to ensure there is just one Database instance
+    static public function getDatabase() {
+    if (self::$_instance == NULL)
+        self::$_instance = new Database();
+    return self::$_instance;
+}
+
+    //this function returns the database connection object
+    public function getConnection() {
+    return $this->objDBConnection;
+}
+
+    //returns the name of the table that stores movies
+    public function getMovieTable() {
+    return $this->param['tblCategories'];
+}
+
+    //returns the name of the table that stores books
+    public function getBookTable() {
+    return $this->param['tblRecipes'];
+}
+
+    //returns the name of the table storing games
+    public function getGameTable() {
+    return $this->param['tblUsers'];
+    }
 }
