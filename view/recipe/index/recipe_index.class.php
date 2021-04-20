7407bd88c167667fadea27500a3c249b9865677a
waretim <82613259+waretim@users.noreply.github.com>
2021-04-20 11:40:48 -0400

    Add files via upload

--- view/recipe/index/recipe_index.class.php
+++ view/recipe/index/recipe_index.class.php
@@ -8,61 +8,50 @@
 
 class RecipeIndex extends RecipeIndexView
 {
+    /*
+     * the display method accepts an array of movie objects and displays
+     * them in a grid.
+     */
 
-    public function display($recipe, $confirm = "") {
+    public function display($recipes) {
         //display page header
-        parent::displayHeader("Recipe Details");
-
-        //retrieve recipe details by calling get methods
-        $id = $recipe->getId();
-        $title = $recipe->getTitle();
-        $category = $recipe->getCategory();
-        $description = $recipe->getDescription();
-        $ingredients = $recipe->geIngredients();
-        $price = $recipe->getPrice();
-        $image = $recipe->getImage();
-
-
-        if (strpos($image, "http://") === false AND strpos($image, "https://") === false) {
-            $image = BASE_URL . '/' . RECIPE_IMG . $image;
-        }
+        parent::displayHeader("List All Recipes");
         ?>
+        <div id="main-header"> Recipes in the Library</div>
 
-        <div id="main-header">Book Details</div>
-        <hr>
-        <!-- display recipe details in a table -->
-        <table id="detail">
-            <tr>
-                <td style="width: 150px;">
-                    <img src="<?= $image ?>" alt="<?= $title ?>" />
-                </td>
-                <td style="width: 130px;">
-                    <p><strong>Title:</strong></p>
-                    <p><strong>Category:</strong></p>
-                    <p><strong>Description:</strong></p>
-                    <p><strong>Ingredients:</strong></p>
-                    <p><strong>Price:</strong></p>
-                    <div id="button-group">
-                        <input type="button" id="edit-button" value="   Edit   "
-                               onclick="window.location.href = '<?= BASE_URL ?>/recipe/edit/<?= $id ?>'">&nbsp;
-                    </div>
-                </td>
-                <td>
-                    <p><?= $title ?></p>
-                    <p><?= $category ?></p>
-                    <p><?= $description->format('m-d-Y') ?></p>
-                    <p><?= $ingredients ?></p>
-                    <p><?= $price ?></p>
-                    <p class="media-description"><?= $description ?></p>
-                    <div id="confirm-message"><?= $confirm ?></div>
-                </td>
-            </tr>
-        </table>
-        <a href="<?= BASE_URL ?>/recipe/index">Go to recipe list</a>
+        <div class="grid-container">
+            <?php
+            if ($recipes === 0) {
+                echo "No recipe was found.<br><br><br><br><br>";
+            } else {
+                //display movies in a grid; six movies per row
+                foreach ($recipes as $i => $recipe) {
+                    $id = $recipe->getId();
+                    $title = $recipe->getTitle();
+                    $category = $recipe->getCategory();
+                    $description = $recipe->getDescription();
+                    $image = $recipe->getImage();
+                    if (strpos($image, "http://") === false AND strpos($image, "https://") === false) {
+                        $image = BASE_URL . "/" . FOOD_IMG . $image;
+                    }
+                    if ($i % 6 == 0) {
+                        echo "<div class='row'>";
+                    }
+
+                    echo "<div class='col'><p><a href='", BASE_URL, "/recipe/detail/$id'><img src='" . $image .
+                        "'></a><span>$title<br>Rated $category<br>" . $description() . "</span></p></div>";
+                    ?>
+                    <?php
+                    if ($i % 6 == 5 || $i == count($recipes) - 1) {
+                        echo "</div>";
+                    }
+                }
+            }
+            ?>
+        </div>
 
         <?php
         //display page footer
         parent::displayFooter();
-    }
-
+    } //end of display method
 }
