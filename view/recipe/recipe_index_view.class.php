80fbfd4ff68edaeed9a3854d952db796fe08e87f
waretim <82613259+waretim@users.noreply.github.com>
2021-04-20 11:40:34 -0400

    Add files via upload

--- view/recipe/recipe_index_view.class.php
+++ view/recipe/recipe_index_view.class.php
@@ -1,12 +1,33 @@
 <?php
 /**
- * Author: Sydney Weddell
- * Date: 4/15/2021
+ * Author: Tim Ware Jr.
+ * Date: 4/20/2021
  * File: recipe_index_view.class.php
  * Description:
  */
 
-class RecipeIndexView
+class RecipeIndexView extends IndexView
 {
+    public static function displayHeader($title) {
+        parent::displayHeader($title)
+        ?>
+        <script>
+            //the media type
+            var food = "recipe";
+        </script>
+        <!--create the search bar -->
+        <div id="searchbar">
+            <form method="get" action="<?= BASE_URL ?>/recipe/search">
+                <input type="text" name="query-terms" id="searchtextbox" placeholder="Search recipes by title" autocomplete="off" onkeyup="handleKeyUp(event)">
+                <input type="submit" value="Go" />
+            </form>
+            <div id="suggestionDiv"></div>
+        </div>
+        <?php
+    }
+
+    public static function displayFooter() {
+        parent::displayFooter();
+    }
 
 }
